c90f2615cc806ea5b0c12c23820e8b9500915e70
ben <unknown>
2024-03-06 14:26:00 +0000

    feat: template and parsing much more refined

--- long_document/generate_long_document.py
+++ long_document/generate_long_document.py
@@ -4,43 +4,45 @@ import re
 from rlextra.rml2pdf import rml2pdf
 
 
-def load_aow():
+def load_document(filename):
     data = {
-        "title": "The Art of War",
-        "author": "Sun Tzu",
         "header": "",
-        "chapters": {},
+        "title": "",
+        "author": "",
+        "translator": "",
+        "chapters": [],
         "copyright": "",
     }
-    roman_numerals_regex = '(X\\.|V\\.|IX\\.|IV\\.|V?I{1,3}\\.|X?I{1,3}\\.)'
-    copyright_delimiter = '----------------------------------------------------------------------'
-    regex = f'{roman_numerals_regex}|{copyright_delimiter}'
-    split_text = re.split(regex, open("artwar.1b.txt", "r").read())
-    for i, text in enumerate(split_text):
-        if text:
-            if i == 0:
-                start_idx = text.index("<html>")
-                data['header'] = text[start_idx:].strip("<html><body><pre>\n")
-            elif i == len(split_text) - 1:
-                data['copyright'] = text.strip("\n\n").strip("\n</pre></body></html>")
-            elif re.match(regex, text) != None:
-                splitter = "\n\n"
-                chapter = split_text[i+1]
-                title_break_index = chapter.index("\n\n")
-                # replace single newlines with \1 which targets the first capture group followed by a blank space
-                replace_1_newline = re.sub('(.)\n(?!\n)', r'\1 ', chapter[title_break_index+len(splitter):])
-                data["chapters"][f"{text} {chapter[:title_break_index]}"] = replace_1_newline
+    delimiter = '----------------------------------------------------------------------'
+    split_text = open(filename, "r").read().split(delimiter)
+    key_info, chapters, copyright_info = split_text[0], split_text[1:-1], split_text[-1]
+    data['copyright'] = copyright_info.strip("\n\n")
+    key_info_split = key_info.split("\n\n")
+    for i, info in enumerate(key_info_split):
+        if i == 0:
+            data['header'] = info
+        elif i == 1:
+            title, author = info.split("\n")
+            data['title'] = title
+            data['author'] = author.replace("By ", "")
+        elif i == 2:
+            data['translator'] = info.strip("\n")
+    for chapter in chapters:
+        chapter_split = chapter.split("\n\n")
+        chapter_title = chapter_split[1]
+        chapter_body = chapter_split[2:]
+        data['chapters'].append((chapter_title, chapter_body))
     return data
 
 
 def main():
-    art_of_war = load_aow()
-    namespace = {'art_of_war': art_of_war}
+    document = load_document("artwar.1b.txt")
+    namespace = {'document': document}
     template_file_name = 'longdocument.prep'
     template = preppy.getModule(template_file_name, savePyc=False, importModule=False)
     rml = template.getOutput(namespace)
-    output_pdf_name = 'art_of_war.pdf'
-    rml2pdf.go(rml.encode(), outputFileName=output_pdf_name)
+    output_pdf_name = "_".join(document['title'].lower().split(" ")) + '.pdf'
+    rml2pdf.go(rml.encode(), outputFileName=output_pdf_name, saveRml="latest.rml")
     print(f"Generated {output_pdf_name} from {template_file_name}")
 
 
