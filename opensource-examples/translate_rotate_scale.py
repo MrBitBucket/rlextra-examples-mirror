c61a2b9cb6da5141800f723efbd52a9b387a0d3c
ben <unknown>
2024-08-06 15:37:36 +0100

    chore: polished the translate_rotate_scale example

--- opensource-examples/translate_rotate_scale.py
+++ opensource-examples/translate_rotate_scale.py
@@ -9,8 +9,10 @@ MEDIA_DIR = "media"
 
 def transformations(canvas):
     """Setting transformations apply to all the images that come after, so you must invert them to add normally again"""
+    logo = os.path.join(MEDIA_DIR, 'reportlab.png')
+    canvas.setFont("Helvetica", 20, leading = None)
     # Translate
-    logo = os.path.join(MEDIA_DIR, 'translate.png')
+    canvas.drawString(x=30, y=780, text="Translate")
     canvas.drawInlineImage(image=logo, x=100, y=700, width=50, height=50)
     canvas.translate(dx=55, dy=0) #Adding transformation so you can see the underlying image
     canvas.drawInlineImage(image=logo, x=100, y=700, width=50, height=50)
@@ -18,7 +20,7 @@ def transformations(canvas):
 
     # Rotate
     # It's rotating around the page origin 0, 0. So we must translate to the image location first so we rotate around it
-    logo = os.path.join(MEDIA_DIR, 'rotate.png')
+    canvas.drawString(x=30, y=580, text="Rotate")
     canvas.drawInlineImage(image=logo, x=100, y=500, width=50, height=50)
     # Rotate clockwise 90
     canvas.translate(dx=100, dy=500)
@@ -32,10 +34,10 @@ def transformations(canvas):
 
     # Scale
     # Also applies to x and y value, so this should be combined with translate. The image has same parameters each time.
-    logo = os.path.join(MEDIA_DIR, 'scale.png')
-    canvas.drawInlineImage(image=logo, x=100, y=200, width=50, height=50)
+    canvas.drawString(x=30, y=380, text="Scale")
+    canvas.drawInlineImage(image=logo, x=100, y=180, width=50, height=50)
     canvas.scale(1.5, 1.5)
-    canvas.drawInlineImage(image=logo, x=100, y=200, width=50, height=50)
+    canvas.drawInlineImage(image=logo, x=100, y=180, width=50, height=50)
     canvas.scale(1/1.5, 1/1.5)
 
 
