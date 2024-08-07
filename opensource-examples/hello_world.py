b9128a113f5e06f4f1ab41a90e0767d431febd02
ben <unknown>
2024-08-07 12:05:24 +0100

    refactor: adjusting translate_rotate_scale and hello_world examples

--- opensource-examples/hello_world.py
+++ opensource-examples/hello_world.py
@@ -1,29 +1,19 @@
-from reportlab.lib.styles import getSampleStyleSheet,ParagraphStyle
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.rl_config import defaultPageSize
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import inch
-PAGE_HEIGHT=defaultPageSize[1]; PAGE_WIDTH=defaultPageSize[0]
 
-styles = getSampleStyleSheet()
 
-Title = "Hello world Heading"
-pageinfo = "platypus example"
-def myFirstPage(canvas, doc):
-	canvas.saveState()
-	canvas.setFont('Times-Bold',16)
-	canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-108, Title)
-	canvas.setFont('Times-Roman',9)
-	canvas.drawString(inch, 0.75 * inch, "First Page / %s" % pageinfo)
-	canvas.restoreState()
-	
-def go():
-	doc = SimpleDocTemplate("hello-world.pdf")
-	Story = [Spacer(1,1*inch)]
-	style = styles["Normal"]
-	text = "Hello World Text"
-	p = Paragraph(text, style)
-	Story.append(p)
-	doc.build(Story, onFirstPage=myFirstPage)
+def first_page():
+	c = canvas.Canvas("hello-world.pdf", pagesize=A4)
+	width, height = A4
+	c.saveState()
+	c.setFont('Times-Bold',16)
+	c.drawCentredString(width/2.0, height-108, "Hello world Heading")
+	c.setFont('Times-Roman',12)
+	c.drawString(100, 700, "Hello World Text")
+	c.restoreState()
+	c.showPage()
+	c.save()
 
-go()
+
+first_page()
--- opensource-examples/translate_rotate_scale.py
+++ opensource-examples/translate_rotate_scale.py
@@ -1,51 +1,42 @@
 import os
-
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
 
-MEDIA_DIR = "media"
+def transformations():
+    # Setting transformations apply to canvas and effects all added after, so use saveState and restoreState.
+    c = canvas.Canvas("translate_rotate_scale.pdf", pagesize=A4)  # 595 x 842 is the page dimensions for A4 at 72DPI
+    logo = os.path.join('media', 'reportlab.png')
+    c.setFont("Helvetica", 20, leading=None)
 
-
-def transformations(canvas):
-    """Setting transformations apply to all the images that come after, so you must invert them to add normally again"""
-    logo = os.path.join(MEDIA_DIR, 'reportlab.png')
-    canvas.setFont("Helvetica", 20, leading = None)
     # Translate
-    canvas.drawString(x=30, y=780, text="Translate")
-    canvas.drawInlineImage(image=logo, x=100, y=700, width=50, height=50)
-    canvas.translate(dx=55, dy=0) #Adding transformation so you can see the underlying image
-    canvas.drawInlineImage(image=logo, x=100, y=700, width=50, height=50)
-    canvas.translate(dx=-55, dy=0) #Adding transformation so you can see the underlying image
+    c.saveState()
+    c.drawString(x=30, y=780, text="Translate")
+    c.drawInlineImage(image=logo, x=100, y=700, width=50, height=50)
+    c.translate(dx=55, dy=0)  # Adding transformation so you can see the underlying image
+    c.drawInlineImage(image=logo, x=100, y=700, width=50, height=50)
+    c.restoreState()
 
     # Rotate
-    # It's rotating around the page origin 0, 0. So we must translate to the image location first so we rotate around it
-    canvas.drawString(x=30, y=580, text="Rotate")
-    canvas.drawInlineImage(image=logo, x=100, y=500, width=50, height=50)
-    # Rotate clockwise 90
-    canvas.translate(dx=100, dy=500)
-    canvas.rotate(-90)
-    canvas.translate(dx=-100, dy=-500)
-    canvas.drawInlineImage(image=logo, x=100, y=500, width=50, height=50)
-    # Rotate anticlockwise 90
-    canvas.translate(dx=100, dy=500)
-    canvas.rotate(90)
-    canvas.translate(dx=-100, dy=-500)
+    c.drawString(x=30, y=580, text="Rotate")
+    c.drawInlineImage(image=logo, x=100, y=500, width=50, height=50)
+    c.saveState()
+    c.translate(dx=100, dy=500)
+    c.rotate(-90)  # Rotates whole canvas, so translate to make dx, dy the new 0,0
+    c.translate(dx=-100, dy=-500)
+    c.drawInlineImage(image=logo, x=100, y=500, width=50, height=50)
+    c.restoreState()
 
     # Scale
-    # Also applies to x and y value, so this should be combined with translate. The image has same parameters each time.
-    canvas.drawString(x=30, y=380, text="Scale")
-    canvas.drawInlineImage(image=logo, x=100, y=180, width=50, height=50)
-    canvas.scale(1.5, 1.5)
-    canvas.drawInlineImage(image=logo, x=100, y=180, width=50, height=50)
-    canvas.scale(1/1.5, 1/1.5)
+    c.saveState()
+    c.drawString(x=30, y=380, text="Scale")
+    c.drawInlineImage(image=logo, x=100, y=180, width=50, height=50)
+    c.scale(1.5, 1.5)
+    c.drawInlineImage(image=logo, x=100, y=180, width=50, height=50)
+    c.restoreState()
 
-
-def go():
-    c = canvas.Canvas("translate_rotate_scale.pdf", pagesize=A4) # 595 x 842 is the page dimensions for A4 at 72DPI
-    transformations(canvas=c)
     c.showPage()
     c.save()
 
 
-go()
+transformations()
